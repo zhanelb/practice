15d301a1b8db9d5d374933e0debc2a7850eacbfe
Жанель Бейсен <zanelbejsen@MacBook-Air-Zanel.local>
2026-01-25 22:55:30 +0500

    Add examples: input/output, variables, strings

---
+++ w3schools/strings.py
@@ -0,0 +1,7 @@
+s = "hello world"
+print(s.upper())
+print(len(s))
+print(s.replace("world", "Python"))
+a = input("First: ")
+b = input("Second: ")
+print(a, b, sep="***")
---
+++ w3schools/variables.py
@@ -0,0 +1,5 @@
+x = 10
+y = 5
+print(x + y)
+text = "Python"
+print(text)
